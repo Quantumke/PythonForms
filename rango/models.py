7e338ac96ddb852869eb657abe65485f2eb895ea
Quantumke <nguruben@gmail.com>
2015-11-23 21:03:25 +0300

    added models

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class forms(models.Model):
+	username = models.CharField(max_length=100, unique=False)
+	email = models.CharFiels(max_length=100, unique=False)
+	school = models.CharField(max_length=100, unique=False)
+	form = models.CharField(max_length=100, unique=False)
+	date = models.CharField(max_length=100, unique=False)
+	gender = models.CharField(max_length=100, unique=False)
+	password = models.CharField(max_length=100, unique=False)
+	def __unicode__(self):
+		return '%s'%self.username
