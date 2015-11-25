f0170e49e3d9194b23b42a7cb31b994dc5d53042
Ben <Quantumke@users.noreply.github.com>
2015-11-25 23:22:08 +0300

    Update forms.py

--- rango/forms.py
+++ rango/forms.py
@@ -4,13 +4,7 @@ from django import forms
 
 class register(forms.Form):
 
-	username = forms.CharField(max_length=100)
-	email = forms.CharField(max_length=100)
-	school = forms.CharField(max_length=100)
-	form = forms.CharField(max_length=100)
-	date = forms.CharField(max_length=100)
-	gender= forms.CharField(max_length=100)
-	password=forms.CharField(max_length=100)
+
 	class Meta:
 		models = forms
 		fields = ('username', 'email', 'school', 'form', 'date', 'gender', 'password')
